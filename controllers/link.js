67a4fc3d0a040787386c3c2f93dcf78a3e7105fc
keigu <kate.gur.ova@yandex.ru>
2025-01-05 16:27:16 +0300

    added duplicated link ckeck

--- controllers/link.js
+++ controllers/link.js
@@ -15,12 +15,25 @@ router.post('/', async function (req, res) {
     const { body } = req;
 
     try {
+        const duplicated = await Link.findOne({ link: body.link });
+        if (duplicated) {
+            throw ({
+                code: 400,
+                message: "Duplicated link",
+            });
+        }
+
         const link = new Link({ id: crypto.randomUUID(), link: body.link, title: body.title, timestamp: Date.now() });
         await link.save();
         res.status(200);
     } catch (error) {
-        console.log(error);
-        res.status(500);
+        if (error.code) {
+            res.status(error.code);
+            res.send(JSON.stringify({ message: error.message}));
+        } else {
+            console.log(error);
+            res.status(500);
+        }
     } finally {
         res.end();
     }
